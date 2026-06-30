ff101f9ca8e00f8087a57148d738a954ad89d5fa
Ruhail1234 <ruhail.abbas719@education.nsw.gov.au>
2026-06-30 13:25:41 +1000

    Changed tal to talk because we needed to test the sync.

--- Tram Timetabling system/Timetable.py
+++ Tram Timetabling system/Timetable.py
@@ -2,7 +2,7 @@ class Timetable:
     def __init__(self, btype, colour, talk):
         self.birdtype = btype
         self.birdcolour = colour
-        self.birdtalk = tal
+        self.birdtalk = talk
 
     def fly(self):
         print("The " + self.birdtype + " is flying.")
