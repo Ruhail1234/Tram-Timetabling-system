ff57f465d9d622a7b3330cd6e4fa0bc7f6b2b597
Ruhail1234 <ruhail.abbas719@education.nsw.gov.au>
2026-07-02 10:57:35 +1000

    Added new methids and attributes

--- Tram Timetabling system/Timetable.py
+++ Tram Timetabling system/Timetable.py
@@ -1,45 +1,16 @@
 class Timetable:
-    def __init__(self, btype, colour, talk):
-        self.birdtype = btype
-        self.birdcolour = colour
-        self.birdtalk = talk
+    def __init__(self, tram_name, arrival_time, max_capacity, passenger_count, stop_names):
+        self.tram_name = tname
+        self.arrival_time = Atime
+        self.max_capacity = Mcapacity
+        self.passenger_count = Passengers
+        self.stop_names = Stops
 
-    def fly(self):
-        print("The " + self.birdtype + " is flying.")
+    def getTramName(self):
+        return tname
 
-    def eat(self):
-        print("The " + self.birdtype + " is eating.")
+    def getArrivalTime(self):
+        ret
     
-    def sleep(self):
-        print("The " + self.birdtype + " is sleeping.")
-    
-    def poo(self):
-        print("The " + self.birdtype + " is pooping.")
-    
-    def talking(self):
-        if self.birdtalk == "can talk":
-            dialogue = input("What do you want to teach the bird to speak")
-            print(self.birdtype + " says " + dialogue)
-        elif self.birdtalk == "can't talk":
-            print(self.birdtype + " can't talk")
-
 Budgerigar = bird("Budgerigar", "blue", "can't talk")
 Budgerigar.fly()
-Budgerigar.eat()
-Budgerigar.sleep()
-Budgerigar.poo()
-Budgerigar.talking()
-
-Kookaburra = bird("Kookaburra", "Brown and white", "can't talk")
-Kookaburra.fly()
-Kookaburra.eat()
-Kookaburra.sleep()
-Kookaburra.poo()
-Kookaburra.talking()
-
-Parrot = bird("Parrot", "Red and Blue", "can talk")
-Parrot.fly()
-Parrot.eat()
-Parrot.sleep()
-Parrot.poo()
-Parrot.talking()
