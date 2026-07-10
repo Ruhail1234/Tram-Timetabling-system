58d2d8fa555f0e566961609b6d7cd39d52b1b52f
Ruhail1234 <ruhail.abbas719@education.nsw.gov.au>
2026-07-10 12:42:16 +1000

    Fixed bugs, and added new inhertiance called Harbour_Town

--- Tram Timetabling system/Timetable.py
+++ Tram Timetabling system/Timetable.py
@@ -1,10 +1,10 @@
-class Timetable:
-    def __init__(self, tram_name, arrival_time, max_capacity, passenger_count, stop_names=None):
+class Timetable_Model:
+    def __init__(self, tram_name, arrival_time, max_capacity, passenger_count, stop_names):
         self.tram_name = tram_name
         self.arrival_time = arrival_time
         self.max_capacity = max_capacity
         self.passenger_count = passenger_count
-        self.stop_names = stop_names if stop_names is not None else []
+        self.stop_names = stop_names
 
     def getTramName(self):
         print("Tram Name: ", self.tram_name)
@@ -20,10 +20,10 @@ class Timetable:
         print("No more than ", self.max_capacity, " Passengers")
 
     def PassengerCount(self):
-        print(self.PassengerCount)
+        print(self.Passenger_count)
 
     def updatePassengerCount(self):
-        self.passenger_count + 1
+        self.passenger_count += 1
     
     def getStopNames(self):
         print("Stopping at: ", self.stop_names)
@@ -37,18 +37,30 @@ class Timetable:
 
     def removeStop(self):
         stop_remove = input("What value of stop due you want to remove")
-        
-        self.stop_names.pop(stop_remove)
+        if stop_remove in self.stop_names:
+            self.stop_names.remove(stop_remove)
+        else:
+            print("Stop not found")
 
     def isfull(self):
-        if self.passenger_count > 2800:
-            print("Beware: Tram is full.")
+        if self.passenger_count >= self.max_capacity:
+            print("Tram is full")
         else:
-            print("Tram is not full")
+            print("All aboard!")
+        
 
-    
-Leppingotn = Timetable("Leppington", "5:30am", "2800", "0", ["Canvely Vale", "Cabramatta", "Warwick Farm", "Liverpool", "Casula", "Glenfield", "Edminson Park", "Leppington"])
-Leppingotn.getTramName()
-Leppingotn.getArrivalTime()
-Leppingotn.getStopNames()
-Leppingotn.getMaxCapacity()
+Leppingoton = Timetable_Model("Leppington", "5:30am", 2800, 0, ["Canley Vale", "Cabramatta", "Warwick Farm", "Liverpool", "Casula", "Glenfield", "Edminson Park", "Leppington"])
+Leppingoton.getTramName()
+Leppingoton.removeStop()
+Leppingoton.getArrivalTime()
+Leppingoton.getStopNames()
+Leppingoton.getMaxCapacity()
+Leppingoton.isfull()
+
+Harbour_Town = Timetable_Model("Harbour Town", "5:37am", 2800, 0, ["BanksTown", "Meryion", "Quackers hill", "Scofield", "Harbour Town"])
+Harbour_Town.getTramName()
+Harbour_Town.removeStop()
+Harbour_Town.getArrivalTime()
+Harbour_Town.getStopNames()
+Harbour_Town.getMaxCapacity()
+Harbour_Town.isfull()
