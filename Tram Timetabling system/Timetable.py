7ef0788a2783c450c9548da5ee3eb468caa66d5f
Hassibazim <hassib.azim529@education.nsw.gov.au>
2026-07-27 10:20:07 +1000

    improved the whole code

--- Tram Timetabling system/Timetable.py
+++ Tram Timetabling system/Timetable.py
@@ -1,3 +1,8 @@
+print("============================================================")
+print("          🚊 SYDNEY TRAMS TIMETABLE SYSTEM 🚊          ")
+print("============================================================")
+print("                 System Initialised...                      \n")
+
 class Timetable_Model:
     def __init__(self, tram_name, arrival_time, max_capacity, passenger_count, stop_id, stop_names):
         self.tram_name = tram_name
@@ -14,14 +19,14 @@ class Timetable_Model:
         print("Arriving at: ", self.arrival_time)
 
     def setArrivalTime(self):
-        new_time = input("What would be the time?")
+        new_time = input("What would be the time? ")
         self.arrival_time = new_time
 
     def getMaxCapacity(self):
         print("No more than ", self.max_capacity, " Passengers")
 
     def PassengerCount(self):
-        print(self.Passenger_count)
+        print(self.passenger_count)
 
     def updatePassengerCount(self):
         self.passenger_count += 1
@@ -30,14 +35,12 @@ class Timetable_Model:
         print("Stopping at: ", self.stop_names)
 
     def addStop(self):
-        newStop = input("what stop do you want to add?")
-
+        newStop = input("what stop do you want to add? ")
         self.stop_names.append(newStop)
-
         print(self.stop_names)
 
     def removeStop(self):
-        stop_remove = input("What value of stop due you want to remove")
+        stop_remove = input("What value of stop do you want to remove? ")
         if stop_remove in self.stop_names:
             self.stop_names.remove(stop_remove)
         else:
@@ -60,19 +63,20 @@ class Timetable_Model:
 Leppington = Timetable_Model("Leppington", "5:30am", 2800, 0, 7123, [])
 Leppington.getTramName()
 Leppington.getStopDetails()
+Leppington.getStopNames()
 Leppington.removeStop()
 Leppington.getArrivalTime()
-Leppington.getStopNames()
 Leppington.getMaxCapacity()
 Leppington.isfull()
+
+print("\n------------------------------------------------------------\n")
 
 Harbour_Town = Timetable_Model("Harbour Town", "5:37am", 2800, 0, 7144, [])
 Harbour_Town.getTramName()
 Harbour_Town.getStopDetails()
+Harbour_Town.getStopNames()
 Harbour_Town.removeStop()
 Harbour_Town.getArrivalTime()
-Harbour_Town.getStopNames()
 Harbour_Town.getMaxCapacity()
 Harbour_Town.isfull()
 
-exit("\033[1mTimeTable brought to you by Sydney Trams\033[0m")
