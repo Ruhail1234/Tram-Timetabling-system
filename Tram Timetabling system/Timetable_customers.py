695f8b6d0c329ac3a7c1302f97dbf523addd694c
Ruhail1234 <ruhail.abbas719@education.nsw.gov.au>
2026-07-28 12:49:30 +1000

    Changed leppington to Carlingford

--- Tram Timetabling system/Timetable_customers.py
+++ Tram Timetabling system/Timetable_customers.py
@@ -70,26 +70,14 @@ class Timetable_Model:
 #This methods gets the details of all the stop for a trams and assigns stops to a tram according to their stop id.
     def getStopDetails(self):
         if self.stop_id == 7123:
-            self.stop_names = ["Canley Vale", "Cabramatta", "Warwick Farm", "Liverpool", "Casula", "Glenfield", "Edminson Park", "Leppington"]
+            self.stop_names = ["Westmead ", "Westmead Hospital", "Childrens Hospital", "Ngara", "Benaud Oval", "Fennell Street", "Prince Alfred Square", "Church Street", "Parramatta Square", "Robin Thomas", "Tramway Avenue", "Rosehill Gardens", "Yallamundi", "Dundas", "Telopea", "Carlingford"]
 
 #This inheritance is an example of the first tram that will have its information displayed.
-Leppington = Timetable_Model("Leppington", "5:30am", 400, 0, 7123, [])
-Leppington.getTramName()
-Leppington.getStopDetails()
-Leppington.getStopNames()
-Leppington.PassengerCount()
-Leppington.getArrivalTime()
-Leppington.getMaxCapacity()
-Leppington.isfull()
-
-print("\n------------------------------------------------------------\n")
-
-#This inheritance is another tram which will have its information displayed on the timetable after it is fully printed.
-Harbour_Town = Timetable_Model("Harbour Town", "5:37am", 400, 0, 7144, [])
-Harbour_Town.getTramName()
-Harbour_Town.getStopDetails()
-Harbour_Town.getStopNames()
-Harbour_Town.PassengerCount()
-Harbour_Town.getArrivalTime()
-Harbour_Town.getMaxCapacity()
-Harbour_Town.isfull()
+Carlingford = Timetable_Model("Carlingford", "5:30am", 400, 0, 7123, [])
+Carlingford.getTramName()
+Carlingford.getStopDetails()
+Carlingford.getStopNames()
+Carlingford.PassengerCount()
+Carlingford.getArrivalTime()
+Carlingford.getMaxCapacity()
+Carlingford.isfull()
